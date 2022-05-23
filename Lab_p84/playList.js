1769a31e87a6b86e50c9c8b507b676988dce9232
JenCHuang <gentoohuang@gmail.com>
2022-05-24 00:36:55 +0800

    20220524-1

--- Lab_p84/playList.js
+++ Lab_p84/playList.js
@@ -1,14 +1,14 @@
 //YouTube Video ID
 let playList = [
-    "SwZMS-ISbH4",
-    "HXf4Ltggjfw",
-    "c6gi6IP6ay4"
+    "YjKLsV5Qn50",
+    "ZVEEud27SII",
+    "siQJhIp-UTU"
 ];
 // 請自己改變內容, 找尋可以嵌入撥放的曲目
 
 //播放起訖秒數
 let playTime = [
-    [101,109],
-    [144,150],
-    [146,152]
+    [68,78],
+    [146,156],
+    [212,229]
 ];
